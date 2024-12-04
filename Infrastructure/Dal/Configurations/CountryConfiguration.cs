09df821fd130156f006683d5a4eead12b161399d
DEX2207 <drovosekleg228@gmail.com>
2024-12-04 15:59:09 +0200

    Homework 27.11.24

---
+++ Infrastructure/Dal/Configurations/CountryConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Dal.Configurations;
+
+/// <summary>
+/// Конфигурация Country для базы данных
+/// </summary>
+
+public class CountryConfiguration:IEntityTypeConfiguration<Country>
+{
+    public void Configure(EntityTypeBuilder<Country> builder)
+    {
+        builder.ToTable(nameof(Country));
+
+        builder.HasKey(c => c.Id);
+
+        builder.Property(c => c.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(c => c.Code)
+            .IsRequired()
+            .HasMaxLength(2)
+            .IsFixedLength();
+        
+        builder.HasMany(x => x.Drugs)
+            .WithOne(d => d.Country)
+            .HasForeignKey(d => d.CountryCodeID)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
---
+++ Infrastructure/Dal/Configurations/DrugItemConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Dal.Configurations;
+
+public class DrugItemConfiguration : IEntityTypeConfiguration<DrugItem>
+{
+    public void Configure(EntityTypeBuilder<DrugItem> builder)
+    {
+        builder.ToTable(nameof(DrugItem));
+
+        builder.HasKey(di => di.Id);
+
+        builder.Property(di => di.Cost)
+            .IsRequired()
+            .HasColumnType("decimal(18,2)")
+            .HasDefaultValue(0);
+
+        builder.Property(di => di.Count)
+            .IsRequired()
+            .HasDefaultValue(0);
+
+        builder.Property(di => di.DrugID)
+            .IsRequired();
+
+        builder.Property(di => di.DrugStoreID)
+            .IsRequired();
+
+        builder.HasOne(di => di.DrugStore)
+            .WithMany(ds => ds.DrugItems)
+            .HasForeignKey(di => di.DrugStore)
+            .OnDelete(DeleteBehavior.Cascade); 
+
+        builder.HasOne(di => di.Drug)
+            .WithMany()
+            .HasForeignKey(di => di.DrugStore)
+            .OnDelete(DeleteBehavior.Restrict); 
+    }
+}
